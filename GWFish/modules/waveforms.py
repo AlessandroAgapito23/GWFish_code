58ce78993b90fcf075b25aa850c8f8d477c52ea6
AlessandroAgapito23 <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-02 13:03:40 +0100

    Update waveforms.py

--- GWFish/modules/waveforms.py
+++ GWFish/modules/waveforms.py
@@ -1320,7 +1320,7 @@ class IMRPhenomD_PPE(Waveform):
         # Interpolate from dataset to evaluate damping and ringdown frequencies
         chi_f, m_f = final_bh(M1, M2, chi_1, chi_2)
     
-        data_ff = np.loadtxt(os.path.dirname(gw.__file__)+'/IMRPhenomD_PPE_n1l2m2.dat', unpack = True)
+        data_ff = np.loadtxt('/content/IMRPhenomD_PPE_n1l2m2.dat', unpack = True)
         M_omega = interp1d(data_ff[0, :], data_ff[1, :])
         tau_omega = interp1d(data_ff[0, :], data_ff[2, :])
     
