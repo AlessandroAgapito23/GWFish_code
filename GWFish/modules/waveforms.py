08bf84a4e242f939e15536ea2c7bd9313384eee3
AlessandroAgapito23 <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-08 18:50:15 +0100

    Update waveforms.py

--- GWFish/modules/waveforms.py
+++ GWFish/modules/waveforms.py
@@ -1244,7 +1244,7 @@ class TaylorF2_PPE(Waveform):
                 phi_2*delta_phi_2*(np.pi*ff)**(-1.) +\
                 phi_3*delta_phi_3*(np.pi*ff)**(-2./3.) +\
                 phi_4*delta_phi_4*(np.pi*ff)**(-1./3.) +\
-                phi_5*delta_phi_5* +\
+                phi_5*delta_phi_5 +\
                 phi_6*delta_phi_6*(np.pi*ff)**(1./3.) +\
                 phi_7*delta_phi_7*(np.pi*ff)**(2./3.)) 
         
