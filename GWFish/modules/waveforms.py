37403beab707ab77c030c67e4362c2c3104138f8
AlessandroAgapito23 <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-08 12:29:09 +0100

    Update waveforms.py

--- GWFish/modules/waveforms.py
+++ GWFish/modules/waveforms.py
@@ -1632,8 +1632,8 @@ class IMRPhenomD_PPE(Waveform):
     
             
         # Conjunction frequencies
-        #f3_amp = (np.abs(ff_RD + (ff_damp*gamma3*(np.sqrt(1-gamma2**2.) - 1)/gamma2)))
-        f3_amp = ff_RD
+        f3_amp = (np.abs(ff_RD + (ff_damp*gamma3*(np.sqrt(1-gamma2**2.) - 1)/gamma2)))
+        #f3_amp = ff_RD
         f2_amp = (f1_amp + f3_amp)/2.
 
         
